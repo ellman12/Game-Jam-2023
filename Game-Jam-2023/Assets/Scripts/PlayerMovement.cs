e82b80d7d772479972a9a80f8d6195d11390f68f
TrojanGD <73033572+obliterine@users.noreply.github.com>
2023-02-03 20:34:15 -0600

    Acceleration go brr
    Made an acceleration at the beginning, shit kinda go hard methinks.

--- Game-Jam-2023/Assets/Scripts/PlayerMovement.cs
+++ Game-Jam-2023/Assets/Scripts/PlayerMovement.cs
@@ -14,16 +14,35 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 direction;
     private bool sprinting;
-    
+
+    [SerializeField]
+    private AnimationCurve acceleration;
+    private float curveIndex = 0f;
+    private bool bAccel;
+    private bool bDecel;
 
     private void Start()
     {
         pInput = new PlayerInput();
         pInput.Enable();
 
+        pInput.PlayerMovement.WASD.performed += startAccel;
+        pInput.PlayerMovement.WASD.canceled += stopAccel;
+
         pInput.PlayerMovement.Sprint.performed += StartSprint;
         pInput.PlayerMovement.Sprint.canceled += StopSprint;
     }
+
+    private void startAccel(InputAction.CallbackContext c)
+    {
+        bAccel = true;
+    }
+    private void stopAccel(InputAction.CallbackContext c)
+    {
+        bAccel = false;
+        curveIndex = 0f;
+    }
+
     private void StartSprint(InputAction.CallbackContext c)
     {
         sprinting = true;
@@ -36,6 +55,12 @@ public class PlayerMovement : MonoBehaviour
     private void OnDisable()
     {
         pInput.Disable();
+
+        pInput.PlayerMovement.WASD.performed -= startAccel;
+        pInput.PlayerMovement.WASD.canceled -= stopAccel;
+
+        pInput.PlayerMovement.Sprint.performed -= StartSprint;
+        pInput.PlayerMovement.Sprint.canceled -= StopSprint;
     }
 
     private void Update()
@@ -44,13 +69,22 @@ public class PlayerMovement : MonoBehaviour
         direction.y = 0; //y not used for position
         direction.z = 0; //z never used
 
+        if (bAccel)
+        {
+            curveIndex += Time.deltaTime;
+            if (curveIndex >= 0.5f)
+            {
+                curveIndex = 0.5f;
+            }
+        }
+
         if (sprinting)
         {
-            transform.position += direction * Time.deltaTime * speed * sprintCoefficient;
+            transform.position += direction * Time.deltaTime * speed * acceleration.Evaluate(curveIndex) * sprintCoefficient;
         }
         else
         {
-            transform.position += direction * Time.deltaTime * speed;
+            transform.position += direction * Time.deltaTime * speed * acceleration.Evaluate(curveIndex);
         }
 
         if(pInput.PlayerMovement.WASD.ReadValue<Vector2>().y == -1)
