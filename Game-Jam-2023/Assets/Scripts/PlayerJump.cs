a6ce79969c5c3c3932db3eff77259975a088460a
Elliott <ellduc4@gmail.com>
2023-02-04 16:20:01 -0600

    Clean up PlayerJump a bit and fix double jump bug

--- Game-Jam-2023/Assets/Scripts/PlayerJump.cs
+++ Game-Jam-2023/Assets/Scripts/PlayerJump.cs
@@ -1,55 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerJump : MonoBehaviour
 {
-    public float jumpSpeed = 8f;
-    private float direction = 0f;
-    [SerializeField]
-    private Rigidbody2D player;
-    private bool jump, dJump;
-    private PlayerInput pInput;
-    [SerializeField]
-    private LayerMask mask;
-    // Start is called before the first frame update
-    void Start()
-    {
-        pInput = new PlayerInput();
-        pInput.Enable();
-        player = GetComponent<Rigidbody2D>();
+	[SerializeField] private Rigidbody2D player;
+	[SerializeField] private LayerMask jumpLayerMask;
+	
+	private bool jump, dJump;
+	private PlayerInput pInput;
+	
+	public float jumpSpeed = 8f;
 
-        pInput.PlayerMovement.Jump.performed += JumpStart;
-    }
+	private void Start()
+	{
+		pInput = new PlayerInput();
+		pInput.Enable();
+		player = GetComponent<Rigidbody2D>();
 
-    private void OnDisable()
-    {
-        pInput.PlayerMovement.Jump.performed -= JumpStart;
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 1.2f);
-        direction = Input.GetAxis("Horizontal");
+		pInput.PlayerMovement.Jump.performed += JumpStart;
+	}
 
-        if (jump && hit.collider != null)
-        {
-            Debug.Log("Working");
-            jump = false;
-            player.velocity = new Vector2(player.velocity.x, jumpSpeed);
-            dJump = true;
-        }
-        if (dJump && jump)
-        {
-            jump = false;
-            player.velocity = new Vector2(player.velocity.x, jumpSpeed);
-            dJump = false;
-        }
-    }
-    private void JumpStart(InputAction.CallbackContext c)
-    {
-        jump = true;
-    }
+	private void OnDisable()
+	{
+		pInput.PlayerMovement.Jump.performed -= JumpStart;
+	}
+
+	private void Update()
+	{
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 1.2f, jumpLayerMask);
+
+		if (jump && hit.collider != null)
+		{
+			Debug.Log("Working");
+			jump = false;
+			player.velocity = new Vector2(player.velocity.x, jumpSpeed);
+			dJump = true;
+		}
+
+		if (dJump && jump)
+		{
+			jump = false;
+			player.velocity = new Vector2(player.velocity.x, jumpSpeed);
+			dJump = false;
+		}
+	}
+
+	private void JumpStart(InputAction.CallbackContext c)
+	{
+		jump = true;
+	}
 }
