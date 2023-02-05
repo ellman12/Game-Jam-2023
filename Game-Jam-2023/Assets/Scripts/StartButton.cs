7b92dc869a2e1121bedc576a51fca3043eb1ab50
Obliterine <73033572+obliterine@users.noreply.github.com>
2023-02-05 12:01:24 -0600

    Added Music
    I added the music and changed the Start script to incorporate the menumusic

--- Game-Jam-2023/Assets/Scripts/StartButton.cs
+++ Game-Jam-2023/Assets/Scripts/StartButton.cs
@@ -3,16 +3,21 @@ using UnityEngine.EventSystems;
 
 public class StartButton : MonoBehaviour, IPointerClickHandler
 {
+	[Header("Button")]
 	[SerializeField] private GameObject mainMenu, player;
+	[Header("Sound Effects")]
+	[SerializeField] private AudioSource menuMusic;
 
 	private void Start()
 	{
 		player.SetActive(false);
+		
 	}
 
 	public void OnPointerClick(PointerEventData eventData)
 	{
 		mainMenu.SetActive(false);
 		player.SetActive(true);
+		menuMusic.Stop();
 	}
 }
